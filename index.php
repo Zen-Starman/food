ebb44074952e1e2a6ab8d2d4d4bf5c2e87f2f546
Zane Stearman <zstearman@mail.greenriver.edu>
2019-04-15 16:14:46 -0400

    Add index

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *  Practice Routing
+ *  Author: Zane Stearman
+ *  Date:   04/15/2019
+ *  File:   index.php
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//define a default route
+//you can do GET /home/main/hello/
+$f3->route('GET /', function(){
+    //display a view
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//Run Fat-free
+$f3->run(); // ->called the object operator
