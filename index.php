dfa65a2de3aae7f7635654e1cc75168a57f273cb
Zane Stearman <zstearman@mail.greenriver.edu>
2019-04-15 16:39:38 -0400

    add more views

--- index.php
+++ index.php
@@ -24,9 +24,20 @@ $f3->set('DEBUG', 3);
 //you can do GET /home/main/hello/
 $f3->route('GET /', function(){
     //display a view
-//    $view = new Template();
-//    echo $view->render('views/home.html');
-    echo "<h1>Breakfast Page</h1>";
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->route('GET /breakfast', function(){
+    //display a view
+    $view = new Template();
+    echo $view->render('views/breakfast.html');
+});
+
+$f3->route('GET /lunch', function(){
+    //display a view
+    $view = new Template();
+    echo $view->render('views/lunch.html');
 });
 
 //Run Fat-free
